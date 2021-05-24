d9bd809bd533eb6005a09514db9913320618af08
Mlira02 <liram1995@gmail.com>
2021-05-24 13:23:55 -0500

    Added spring dev tools and added security for dev endpoints

--- src/main/java/com/marcos/springboot/demo/demo/rest/DemoRestController.java
+++ src/main/java/com/marcos/springboot/demo/demo/rest/DemoRestController.java
@@ -1,4 +1,4 @@
-package com.marcos.springboot.demo.rest;
+package com.marcos.springboot.demo.demo.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,5 +12,17 @@ public class DemoRestController
     public String sayHello()
     {
         return "Hello World, Time on the server is currently: " + LocalDateTime.now();
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout()
+    {
+        return "Just running";
+    }
+
+    @GetMapping("/fortune")
+    public String getFortune()
+    {
+        return "Here's your fortune. Have a Good day...";
     }
 }
