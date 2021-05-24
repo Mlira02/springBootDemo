8e16b804010ae469c775260b61bfd4da43bd34ba
Mlira02 <liram1995@gmail.com>
2021-05-24 13:57:48 -0500

    Added new endpoint to demo app properties file

--- src/main/java/com/marcos/springboot/demo/demo/rest/DemoRestController.java
+++ src/main/java/com/marcos/springboot/demo/demo/rest/DemoRestController.java
@@ -1,5 +1,6 @@
 package com.marcos.springboot.demo.demo.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,18 @@ import java.time.LocalDateTime;
 @RestController
 public class DemoRestController
 {
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo()
+    {
+        return "Coach name: " + coachName + ", Team Name: " + teamName;
+    }
+
     @GetMapping("/")
     public String sayHello()
     {
